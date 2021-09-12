a96215f4bf39404e085722e3e4d8c3aea007cafa
GudzD <gudz.da@gmail.com>
2021-09-12 19:17:44 +0300

    hw11

--- src/main/java/ua/gudz/hw9/task1/Planet.java
+++ src/main/java/ua/gudz/hw9/task1/Planet.java
@@ -2,7 +2,10 @@ package ua.gudz.hw9.task1;
 
 interface Planet {
     double G = 6.67 * Math.pow(10, -11);
+
     double accelerationOfGravity();
+
     boolean existsOfAtmosphere();
+
     int whatAverageTemp();
 }
