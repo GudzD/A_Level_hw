081e30aea5984fc129438145dbe9ed1e52cb945a
GudzD <gudz.da@gmail.com>
2021-09-28 22:32:31 +0300

    hw17

--- src/main/java/ua/gudz/hw17/Task5/Box.java
+++ src/main/java/ua/gudz/hw17/Task5/Box.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
- public class Box {
+public class Box {
     int size;
     List<Item> myListOfItems;
 
@@ -13,14 +13,4 @@ import java.util.List;
         this.size = size;
         this.myListOfItems = myListOfItems;
     }
-//   ArrayList<Item> list1 = new ArrayList<>();
-//   ArrayList<Item> list2 = new ArrayList<>();
-//   ArrayList<Item> list3 = new ArrayList<>();
-//   ArrayList<Item> list4 = new ArrayList<>();
-//   ArrayList<Item> list5 = new ArrayList<>();
-//   ArrayList<Item> list6 = new ArrayList<>();
-//   ArrayList<Item> list7 = new ArrayList<>();
-//   ArrayList<Item> list8 = new ArrayList<>();
-//   ArrayList<Item> list9 = new ArrayList<>();
-//   ArrayList<Item> list10 = new ArrayList<>();
 }
